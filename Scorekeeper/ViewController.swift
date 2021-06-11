d09938ef3fdab6b5b79b69f74c98f5461fabfdf2
Natalie Koenig <koenignm@rose-hulman.edu>
2021-06-10 19:44:43 -0500

    Initial Commit

---
+++ Scorekeeper/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Scorekeeper
+//
+//  Created by Natalie Koenig on 6/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
