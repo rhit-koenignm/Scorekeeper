2990e1a38415ba41d0c1c56e77a9c9a927ae5d3e
Natalie Koenig <koenignm@rose-hulman.edu>
2021-06-15 20:31:31 -0500

    Done with appearance and connections, now just to add the code

--- Scorekeeper/ViewController.swift
+++ Scorekeeper/ViewController.swift
@@ -9,6 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var roundLabel: UILabel!
+    @IBOutlet weak var newGameButton: UIButton!
+    @IBOutlet weak var enterRoundButton: UIButton!
+    
+    @IBOutlet weak var player1Field: UITextField!
+    @IBOutlet weak var player2Field: UITextField!
+    @IBOutlet weak var player3Field: UITextField!
+    @IBOutlet weak var player4Field: UITextField!
+    
+    @IBOutlet weak var score1Field: UITextField!
+    @IBOutlet weak var score1View: UITextView!
+    
+    @IBOutlet weak var score2Field: UITextField!
+    @IBOutlet weak var score2View: UITextView!
+    
+    @IBOutlet weak var score3Field: UITextField!
+    @IBOutlet weak var score3View: UITextView!
+    
+    @IBOutlet weak var score4Field: UITextField!
+    @IBOutlet weak var score4View: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
